dca406384a1bbbcafec0b8878fc17b8af9fb53aa
Osman728 <osmanisse29@gmail.com>
2023-12-15 10:01:26 +0000

    link my api to comment section

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,27 +1,5 @@
-// const comments = [
-//   {
-//     name: "Connor Walton",
-//     date: "02/17/2021",
-//     comment:
-//       "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
-//   },
-
-//   {
-//     name: "Emilie Beach",
-//     date: "01/09/2021",
-//     comment:
-//       "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
-//   },
-
-//   {
-//     name: "Miles Acosta",
-//     date: "12/20/2021",
-//     comment:
-//       "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
-//   },
-// ];
 const bandSiteComments = document.querySelector(".comment");
-function createComment(comments){
+function createComment(comments) {
   const commentEl = document.createElement("article");
   const commentAvartar = document.createElement("div");
   const commentAvartarImg = document.createElement("img");
@@ -66,34 +44,23 @@ function createComment(comments){
 let api = new BandSiteApi(myApiKey);
 
 async function renderComment() {
- 
-
   bandSiteComments.innerHTML = "";
   const comments = await api.getComments();
-  
   comments.sort((a, b) => {
     return b.timestamp - a.timestamp;
   });
-
-  comments.forEach((comment) => createComment(comment))
-
-
+  comments.forEach((comment) => createComment(comment));
 }
-
 renderComment();
 
 const formEl = document.querySelector(".Conversation__form");
-
 const handleSubmit = async (event) => {
   event.preventDefault();
   const newComment = {
     name: event.target.name.value,
     comment: event.target.comment.value,
-    // date:new Date().toLocaleDateString("en-Us"),
   };
-
   await api.postComment(newComment);
-
   renderComment();
   formEl.reset();
 };
